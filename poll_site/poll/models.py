479845937357be1822f2c67da179201ba2ff15f3
Nizigiymana-Tech <junkacc830@gmail.com>
2026-05-12 13:27:53 -0400

    Added Homepage Gradient Background

--- poll_site/poll/models.py
+++ poll_site/poll/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class PollInfo(models.Model):
     name = models.CharField(max_length=1000)
+
+    def __str__(self) -> str:
+        return self.name
